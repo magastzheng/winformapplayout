ef0215bc29201bfe297c0be838e2c66f20c9c5fd
Zheng Guoqiang <zgq10722@htsec.com>
2016-07-08 16:54:11 +0800

    Move the enumtype converter from config project

---
+++ Model/EnumType/EnumTypeConverter/EntrustRequestHelper.cs
@@ -0,0 +1,64 @@
+﻿
+using Model.EnumType;
+using System.Text;
+namespace Config.ParamConverter
+{
+    public class EntrustRequestHelper
+    {
+        public static string GetMarketNo(string exchangeCode)
+        {
+            string marketNo = string.Empty;
+            switch (exchangeCode)
+            { 
+                case "SSE":
+                    marketNo = "1";
+                    break;
+                case "SZSE":
+                    marketNo = "2";
+                    break;
+                case "CFFEX":
+                    break;
+                default:
+                    break;
+            }
+
+            return marketNo;
+        }
+
+        public static string GetEntrustDirection(EntrustDirection eDirection)
+        {
+            string direction = string.Empty;
+
+            switch (eDirection)
+            {
+                case EntrustDirection.BuySpot:
+                case EntrustDirection.BuyClose:
+                    {
+                        direction = "1";
+                    }
+                    break;
+                case EntrustDirection.SellSpot:
+                case EntrustDirection.SellOpen:
+                    {
+                        direction = "2";
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return direction;
+        }
+
+        public static string GetEntrustPriceType(EntrustPriceType eEntrustPriceType)
+        {
+            string entrustpricetype = string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append((char)eEntrustPriceType);
+            entrustpricetype = sb.ToString();
+
+            return entrustpricetype;
+        }
+    }
+}
